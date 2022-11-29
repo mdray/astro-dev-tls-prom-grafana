05deaa09dafc64f2819780dbf483ae29f18172ad
Jacob Martinson <jacobm3@gmail.com>
2022-11-29 12:27:51 -0600

    update

---
+++ astro-dev/dags/simple_wide_dag.py
@@ -0,0 +1,20 @@
+from datetime import datetime,timedelta
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.models import Variable
+
+for n in range(10):
+
+    with DAG(
+        "simple_wide2_" + str(n).zfill(3),
+        schedule_interval=timedelta(seconds=60),
+        start_date=datetime(2021, 8, 1),
+        catchup=False,
+        max_active_tasks=2000,
+        concurrency=2000,
+    ):
+        COUNT = int(Variable.get("COUNT"))
+        for i in range(int(COUNT)):
+            t = DummyOperator(task_id=str(i).zfill(4))
+
